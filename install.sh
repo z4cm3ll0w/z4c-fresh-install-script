5ab8fbce26c970bb4f29eb47b8bee15519b46c2e
z4cm3ll0w <104526677+z4cm3ll0w@users.noreply.github.com>
2022-06-26 16:23:56 +0200

    Update install.sh
    updated apt install using a variable and creating directories in home folder

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 #creating folders
-mkdir ./Scripts
-mkdir ./Development
-mkdir ./Tools
-mkdir ./Git
+mkdir ~/Configs
+mkdir ~/Scripts
+mkdir ~/Development
+mkdir ~/Tools
+mkdir ~/Git
 
 #updating system
 sudo apt update && sudo apt upgrade
 
 #installing software from apt
-sudo apt install wireshark -y
-sudo apt install nmap -y
-sudo apt install firefox -y
-sudo apt install keepassxc -y
-sudo apt install tilix -y
-sudo apt install git -y
+software="wireshark nmap firefox keepassxc tilix git openvpn"
+
+sudo apt install $software -y
+
 
 #cloning Git repositories
 cd ./Git
