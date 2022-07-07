670dde6ff2e11a2b159287f2777e33945f77d07e
z4cm3ll0w <104526677+z4cm3ll0w@users.noreply.github.com>
2022-07-07 21:00:21 +0200

    Update install.sh
    Changed comments to echos
    fixed bug

--- install.sh
+++ install.sh
@@ -1,21 +1,32 @@
+  GNU nano 6.2                           Scripts/install.sh                                    
 #!/bin/bash
 
-#creating folders
+echo "creating folders"
 mkdir ~/Configs
 mkdir ~/Scripts
 mkdir ~/Development
 mkdir ~/Tools
 mkdir ~/Git
 
-#updating system
+echo "updating system"
 sudo apt update && sudo apt upgrade
 
-#installing software from apt
+echo "installing software from apt"
 software="wireshark nmap firefox keepassxc tilix git openvpn"
 
 sudo apt install $software -y
 
+#sudo apt install wireshark -y
+#sudo apt install nmap -y
+#sudo apt install firefox -y
+#sudo apt install keepassxc -y
+#sudo apt install tilix -y
+#sudo apt install git -y
+#sudo apt install openvpn -y
 
-#cloning Git repositories
-cd ./Git
+
+
+echo "cloning Git repositories"
+cd ~/Git
 git clone https://github.com/OWASP/Amass.git
+
